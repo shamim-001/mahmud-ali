fd3a50523040c6c25c80f75caba1887e21ba4c6b
shamim-001 <s.ahsan.ds96@gmail.com>
2025-11-29 06:56:28 +0600

    g

--- .next/dev/static/chunks/app_layout_tsx_70331b46._.js
+++ .next/dev/static/chunks/app_layout_tsx_70331b46._.js
@@ -2,8 +2,8 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/[root-of-the-server]__12055ce9._.css",
-  "static/chunks/node_modules__pnpm_253315d9._.js",
-  "static/chunks/_bf56649d._.js"
+  "static/chunks/_bf56649d._.js",
+  "static/chunks/node_modules__pnpm_253315d9._.js"
 ],
     source: "dynamic"
 });
